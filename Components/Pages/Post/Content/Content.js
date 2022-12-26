8b1b7f2662e3a90002e2d82e4a445031c0ccb7de
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-26 14:48:41 +0000

    post page content checkpoint

--- Components/Pages/Post/Content/Content.js
+++ Components/Pages/Post/Content/Content.js
@@ -1,20 +1,26 @@
 import * as S from './Content.styled'
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 
 const Content = () => {
-    const [value, setValue] = useState('');
-    const genHTML = () => {
-        return {
-            __html: `${value}`
-        }
+    const data = {
+        imageURL: '/landing10.jpg',
+        title: 'The Crisis in Cosmology.', 
+        userName: 'Anonymous', 
+        about: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, quam!',
+        postID: '205'
     }
     return (
-        <S.ContentContainer>
-            <ReactQuill theme="snow" value={value} onChange={setValue} />
-            <div className="test">
-                {value}
+        <S.ContentContainer className='center'>
+            <div className="inner content">
+                <div className="meta">
+                    <h1>{data.title}</h1>
+                    <span>by {data.userName}</span>
+                </div>
+                <div className="image" style={{backgroundImage: `url('${data.imageURL}')`}}></div>
+                <div className="text">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error earum, voluptatem amet debitis veniam aperiam esse et atque impedit laudantium, quia consequatur consequuntur dolor illum laborum magni reiciendis perspiciatis temporibus vero aliquam odio repudiandae necessitatibus omnis quidem! Mollitia nobis eligendi, alias ullam eius quia et velit, adipisci sed nostrum, ab natus doloribus dolorem accusamus. Tempora quam neque ducimus expedita magni voluptates accusamus culpa iste, minima atque voluptatem esse porro repellat itaque sequi possimus quibusdam maiores omnis magnam illum rem provident quos ipsa. Quam, non dolor dolorem, nam eum fugiat fugit temporibus et dolores earum repudiandae deserunt, suscipit impedit. Placeat, est.
+                    </p>
+                </div>
             </div>
         </S.ContentContainer>
     );
--- Components/Pages/Post/Content/Content.styled.js
+++ Components/Pages/Post/Content/Content.styled.js
@@ -2,7 +2,35 @@ import styled from "styled-components";
 
 export const ContentContainer = styled.div`
     width: 100%;
-    & .rdw-editor-main{
-        background-color: white;
+    & .content.inner{
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        & .meta{
+            width: 100%;
+            display: flex; flex-direction: column; 
+            margin-bottom: 40px;
+            & h1{
+                font-size: 2.5em; font-weight: 600;
+                margin-bottom: 20px;
+            }
+            & span{
+                width: fit-content;
+                padding: 5px 10px; 
+                background-color: black; color: white;
+                border-radius: 5px;
+            }
+        }
+        & .image{
+            width: 80%; height:clamp(300px,30vw,500px); background-color: black;
+            min-width: 400px;
+            margin-bottom: 40px;
+            border-radius: 5px;
+            background-position: center; background-size: cover;
+            background-repeat: no-repeat;
+        }
+        & .text{
+            & *{line-height: 2}
+        }
     }
 `
