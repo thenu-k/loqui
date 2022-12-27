8e7822d22ab6f966fe16e7c519ee0fc3d9731d2d
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-27 12:10:06 +0000

    rfc

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,16 +2,7 @@
 
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDaJ6HNoWxBipuxbGtvHgFoJtoKJzSvEUU",
-  authDomain: "loqui-132ce.firebaseapp.com",
-  projectId: "loqui-132ce",
-  storageBucket: "loqui-132ce.appspot.com",
-  messagingSenderId: "23798468690",
-  appId: "1:23798468690:web:2ca9be0651718b4c7bcf8b",
-  measurementId: "G-JYVQ0YB48R"
-};
+import { firebaseConfig } from "../firebase.config";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
