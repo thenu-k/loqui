4e76d428b4d8dda212220e5a95583949dd7d08f3
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-27 13:02:38 +0000

    removed config file from gitignore

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDaJ6HNoWxBipuxbGtvHgFoJtoKJzSvEUU",
+    authDomain: "loqui-132ce.firebaseapp.com",
+    projectId: "loqui-132ce",
+    storageBucket: "loqui-132ce.appspot.com",
+    messagingSenderId: "23798468690",
+    appId: "1:23798468690:web:2ca9be0651718b4c7bcf8b",
+    measurementId: "G-JYVQ0YB48R"
+  };
